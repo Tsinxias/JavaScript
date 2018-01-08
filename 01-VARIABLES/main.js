d508eed127071caa561f63e98af391e3da9c371a
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-01-08 11:40:48 +0100

    variables

---
+++ 01-VARIABLES/main.js
@@ -0,0 +1,21 @@
+let windows = false;
+let linux = true;
+let Age = 23;
+let name = 'Dayvid';
+let something = undefined;
+let willBeChanged = null;
+let table = {};
+let basket = [];
+let doStuff = function () {};
+
+let myString = `leopard is in
+the kitchen`;
+let myOtherString = 'leopard is in \
+the bedroom ';
+
+console.log(myString + ' and the other ' + myOtherString + age);
+
+let math = (10+2)/2 + 4*2;
+// Orders : Parenthesis, multiplication/division, addition/substraction
+
+console.log(math);
