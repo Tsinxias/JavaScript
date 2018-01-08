1627b4df4620fc79de643b15061ec6e226703ba7
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-01-08 16:37:09 +0100

    04-fonction

---
+++ 04-FONCTION/main.js
@@ -0,0 +1,27 @@
+function sayHello() {
+  return 'Hello world';
+}
+// first read all code and then do the declaration (appel)
+// => si on le met avant la création de la fonction, ça fonctionne quand même ! o.O
+
+let sayBye = function(times) {
+  for (let i = 0; i <= times; i++) {
+    console.log('Bye world' + i);
+  }
+  return 'Bye world is finished';
+}
+
+let sayHein = () => {
+  return 'Hein World';
+}
+
+console.log(sayHello());
+console.log(sayHein());
+console.log(sayBye(5));
+
+
+function hello(bouh = 'Charlie'/*défault*/) {
+  return bouh;
+}
+
+console.log(hello("Carla"));
